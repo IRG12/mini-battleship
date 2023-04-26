11d9c9d46ecb7c2d7db4b27de26b07d13e8a16c2
Isaiah G <isaiahrg12@gmail.com>
2023-04-26 13:28:48 -0500

    Practical Understanding2
    I'm testing and researching what I do and do not understand. As well as breaking problem down into small digestable questions and then building on my previous inefficient solutions.

--- part1.js
+++ part1.js
@@ -4,19 +4,36 @@ const readlineSync = require("readline-sync");
 const userName = readlineSync.question("Press any key to start the game. ");
 console.log("Hi " + userName + "!");
 
-const grid = [
-  ["Miss", "Hit", "Miss"],
-  ["Hit", "Miss", "Hit"],
-  ["Miss", "Miss", "Miss"],
-];
+// Define the dimensions of the array
+const rows = 3;
+const cols = 3;
 
-const fillGrid = () => {};
+// Create an empty 2D array
+const grid = new Array(rows).fill(null).map(() => new Array(cols).fill(null));
 
-const traverseGrid = () => {
-  for (let row of grid) {
-    for (let column of row) {
-      console.log(column);
+// Define an array of possible ship names
+const shipNames = ["Ship1", "Ship2"];
+
+// Define an array to store which ships have already been placed
+const shipsPlaced = [];
+
+// Loop through each ship name in the shipNames array
+for (let i = 0; i < shipNames.length; i++) {
+  let placed = false;
+
+  // Loop until the current ship has been successfully placed in the array
+  while (!placed) {
+    const row = Math.floor(Math.random() * rows);
+    const col = Math.floor(Math.random() * cols);
+
+    // If the current location in the array is null, place the ship there
+    if (grid[row][col] === null) {
+      grid[row][col] = shipNames[i];
+      shipsPlaced.push(shipNames[i]);
+      placed = true;
     }
   }
-};
-traverseGrid();
+}
+
+// Output the resulting 2D array to the console
+console.log(grid);
