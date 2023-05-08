82629103702153310230d795320a31d98afb8d3d
Isaiah G <isaiahrg12@gmail.com>
2023-05-08 12:57:34 -0500

    Part2
    Solving why I'm getting undefined on line 85

--- part2.js
+++ part2.js
@@ -1,98 +1,78 @@
-// const readlineSync = require("readline-sync");
+const readlineSync = require("readline-sync");
 
-// const placeShipsRandomly = () => {
-//   // Define the dimensions of the array
-//   const rows = 10;
-//   const cols = 10;
+let boardSize = 10;
+let shipLengths = [5, 4, 3, 3, 2];
+let board = Array.from({ length: boardSize }, () =>
+  Array.from({ length: boardSize }, () => 0)
+);
+let shipLengthsHit = {};
+let shipsSunk = 0;
 
-//   // Create an empty 2D array
-//   const grid = new Array(rows).fill(null).map(() => new Array(cols).fill(null));
-
-//   // Define an array of possible ship names
-
-//   // Define an array to store which ships have already been placed
-//   const shipsPlaced = [];
-//   const shipNames = ["Ship1", "Ship2", "Ship3", "Ship4", "Ship5"];
-//   let ship1 = 2;
-//   let ship2 = 3;
-//   let ship3 = 3;
-//   let ship4 = 4;
-//   let ship5 = 5;
-
-//   // Loop through each ship name in the shipNames array
-//   for (let i = 0; i < shipNames.length; i++) {
-//     let placed = false;
-
-//     // Loop until the current ship has been successfully placed in the array
-//     while (!placed) {
-//       const row = Math.floor(Math.random() * rows);
-//       const col = Math.floor(Math.random() * cols);
-
-//       // If the current location in the array is null, place the ship there
-//       if (grid[row][col] === null) {
-//         grid[row][col] = shipNames[i];
-//         shipsPlaced.push(shipNames[i]);
-//         placed = true;
-//       }
-//     }
-//   }
-//   return grid;
-// };
-
-// const startGame = () => {
-//   readlineSync.keyInPause("Press any key to start the game. ");
-// };
-
-// startGame();
-// let board = placeShipsRandomly();
-// console.log(board);
-
-const boardSize = 10;
-const shipLengths = [5, 4, 3, 3, 2];
-const board = Array.from({ length: boardSize }, () => Array(boardSize).fill(0));
-// console.log(board)
-
-function placeShips() {
-  shipLengths.forEach((shipLength) => {
-    let shipPlaced = false;
-    while (!shipPlaced) {
-      const direction = Math.floor(Math.random() * 2);
-      // console.log((boardSize - shipLength + 1))
-      const [x, y] = [
-        direction === 0
-          ? Math.floor(Math.random() * (boardSize - shipLength + 1))
-          : Math.floor(Math.random() * boardSize),
-        direction === 1
-          ? Math.floor(Math.random() * (boardSize - shipLength + 1))
-          : Math.floor(Math.random() * boardSize),
-      ];
-      // console.log([x,y])
-      const intersect = Array.from({ length: shipLength }, (_, j) =>
-        direction === 0 ? board[x + j][y] : board[x][y + j]
-      ).some((val) => val === 1);
-      console.log(intersect);
-      if (!intersect) {
-        Array.from({ length: shipLength }, (_, j) =>
-          direction === 0 ? (board[x + j][y] = 1) : (board[x][y + j] = 1)
-        );
+function placeShip(shipLength) {
+  let shipPlaced = false;
+  while (!shipPlaced) {
+    const direction = Math.floor(Math.random() * 2); // 0 for horizontal, 1 for vertical
+    let x, y;
+    if (direction === 0) {
+      x = Math.floor(Math.random() * (boardSize - shipLength + 1));
+      y = Math.floor(Math.random() * boardSize);
+      if (
+        !board
+          .slice(x, x + shipLength)
+          .some((row) => row.slice(y, y + 1).some((cell) => cell === 1))
+      ) {
+        board.slice(x, x + shipLength).forEach((row) => (row[y] = 1));
+        shipPlaced = true;
+      }
+    } else {
+      x = Math.floor(Math.random() * boardSize);
+      y = Math.floor(Math.random() * (boardSize - shipLength + 1));
+      if (
+        !board
+          .slice(x, x + 1)
+          .some((row) =>
+            row.slice(y, y + shipLength).some((cell) => cell === 1)
+          )
+      ) {
+        board[x]
+          .slice(y, y + shipLength)
+          .forEach((_, i) => (board[x][y + i] = 1));
         shipPlaced = true;
       }
     }
-  });
+  }
 }
 
+function placeShips() {
+  shipLengths.forEach(placeShip);
+}
 placeShips();
-// console.log(board);
-let coordinatesAttacked = [];
-let shipsRemaining = 5;
 
-// Wait for user's response.
+const playAgain = () => {
+  let playAgain = readlineSync.keyInYN(
+    'You have destroyed all battleships. Would you like to play again? Y/N"'
+  );
+  if (!playAgain) {
+    // Key that is not `Y` was pressed.
+    process.exit();
+  }
+  boardSize = 10;
+  shipLengths = [5, 4, 3, 3, 2];
+  board = Array.from({ length: boardSize }, () =>
+    Array.from({ length: boardSize }, () => 0)
+  );
+  shipLengthsHit = {};
+  shipsSunk = 0;
+  startGame();
+};
+
 const startGame = () => {
   readlineSync.keyInPause("Press any key to start the game. ");
   const continueGame = () => {
-    let userInput = readlineSync.question(
-      `Enter a location to strike i.e.: 'A2'. `
-    );
+    const userGuess = () => {
+      return readlineSync.question(`Enter a location to strike i.e.: 'A2'. `);
+    };
+    let guessedLocation = userGuess();
 
     function convertUserInput(input) {
       const letters = "ABCDEFGHIJ";
@@ -100,112 +80,32 @@ const startGame = () => {
       const col = letters.indexOf(input[0].toUpperCase());
       return [row, col];
     }
-    let enteredLocation = convertUserInput(userInput);
-
-    const positionsAttacked = () => {
-      if (coordinatesAttacked.includes(enteredLocation)) {
+    let convertedGuess = convertUserInput(guessedLocation);
+    function fire(x, y) {
+      if (board[x][y] === 0) {
+        console.log("You missed!");
+      } else if (board[x][y] === -1) {
         console.log("You have already picked this location. Miss!");
-        enteredLocation = location(); // update enteredLocation with new input
-        scanWaters(); // positionsAttacks() is called to check if the location has already been attacked.
-      } else {
-        console.log("You have missed! line 74");
-        coordinatesAttacked.push(enteredLocation);
-        enteredLocation = location(); // update enteredLocation with new input
-        scanWaters();
-      }
-    };
-
-    const attack = (row, col) => {
-      if (board[row][col] === "Ship1" || board[row][col] === "Ship2") {
-        coordinatesAttacked.push(enteredLocation);
-        console.log(coordinatesAttacked, "line 84");
-        shipsRemaining--;
-        console.log(
-          `Hit. You have sunk a battleship. ${shipsRemaining} ship remaining.`
-        );
-        if (shipsRemaining === 0) {
-          playAgain();
+        guessedLocation = userGuess(); // // update guessedLocation with new input
+        fire(...convertUserInput(guessedLocation)); // fire on new position
+      } else if (board[x][y] > 0) {
+        const shipId = board[x][y];
+        shipLengthsHit[shipId] = (shipLengthsHit[shipId] || 0) + 1;
+        board[x][y] = -1;
+        const shipLength = shipLengths[shipId - 1];
+        if (shipLengthsHit[shipId] === shipLength) {
+          console.log(`You sunk a ship of length ${shipLength}!`);
+          shipsSunk++;
+          if (shipsSunk === shipLengths.length) {
+            console.log("Congratulations! You have sunk all the battleships!");
+            playAgain();
+          }
         } else {
-          enteredLocation = location();
-          scanWaters(); // scanWaters() is called to continue attack
-          // console.log(board);
+          console.log(`You hit a ship of length ${shipLength}!`);
         }
-      } else if (board[row][col] === null) {
-        positionsAttacked();
       }
-    };
-
-    // console.log(verify, "'verify' line 92");
-
-    const scanWaters = () => {
-      // console.log(board, "line 89");
-      switch (enteredLocation) {
-        case "A1":
-          if (coordinatesAttacked.includes(enteredLocation)) {
-            positionsAttacked();
-          } else {
-            attack(0, 0);
-          }
-          break;
-        case "A2":
-          if (coordinatesAttacked.includes(enteredLocation)) {
-            positionsAttacked();
-          } else {
-            attack(0, 1);
-          }
-          break;
-        case "A3":
-          if (coordinatesAttacked.includes(enteredLocation)) {
-            positionsAttacked();
-          } else {
-            attack(0, 2);
-          }
-          break;
-        case "B1":
-          if (coordinatesAttacked.includes(enteredLocation)) {
-            positionsAttacked();
-          } else {
-            attack(1, 0);
-          }
-          break;
-        case "B2":
-          if (coordinatesAttacked.includes(enteredLocation)) {
-            positionsAttacked();
-          } else {
-            attack(1, 1);
-          }
-          break;
-        case "B3":
-          if (coordinatesAttacked.includes(enteredLocation)) {
-            positionsAttacked();
-          } else {
-            attack(1, 2);
-          }
-          break;
-        case "C1":
-          if (coordinatesAttacked.includes(enteredLocation)) {
-            positionsAttacked();
-          } else {
-            attack(2, 0);
-          }
-          break;
-        case "C2":
-          if (coordinatesAttacked.includes(enteredLocation)) {
-            positionsAttacked();
-          } else {
-            attack(2, 1);
-          }
-          break;
-        case "C3":
-          if (coordinatesAttacked.includes(enteredLocation)) {
-            positionsAttacked();
-          } else {
-            attack(2, 2);
-          }
-          break;
-      }
-    };
-    scanWaters();
+    }
+    fire(convertedGuess);
   };
   continueGame();
 };
